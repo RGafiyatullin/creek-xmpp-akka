d4841b683c07ff64f01a5ab19dbc1d2b529d31da
Roman Gafiyatullin <r.gafiyatullin@me.com>
2017-01-13 16:18:37 +0300

    creek-xml-binary 0.0.5

--- src/test/scala/com/github/rgafiyatullin/creek_xmpp_akka/xmpp_stream/XmppStream2Test.scala
+++ src/test/scala/com/github/rgafiyatullin/creek_xmpp_akka/xmpp_stream/XmppStream2Test.scala
@@ -3,9 +3,14 @@ package com.github.rgafiyatullin.creek_xmpp_akka.xmpp_stream
 import java.net.InetSocketAddress
 
 import akka.actor.{Actor, ActorRefFactory, ActorSystem}
+import akka.event.Logging.LogLevel
 import akka.io.{IO, Tcp}
 import akka.util.Timeout
 import akka.testkit.TestActorRef
+import com.github.rgafiyatullin.creek_xml.common.QName
+import com.github.rgafiyatullin.creek_xml.dom.{CData, Element}
+import com.github.rgafiyatullin.creek_xmpp.protocol.stanza_error.XmppStanzaError
+import com.github.rgafiyatullin.creek_xmpp.protocol.stanzas.jabber_client.IQ
 import com.github.rgafiyatullin.creek_xmpp.streams.StreamEvent
 import com.github.rgafiyatullin.creek_xmpp_akka.xmpp_stream.transports.BinaryXml
 import com.github.rgafiyatullin.owl_akka_goodies.actor_future.ActorStdReceive
@@ -26,6 +31,7 @@ class XmppStream2Test extends FlatSpec with Matchers with ScalaFutures {
     PatienceConfig(timeout = Span(5, Seconds), interval = Span(500, Millis))
 
   implicit val actorSystem = ActorSystem("host-test")
+  actorSystem.eventStream.setLogLevel(LogLevel(4))
   implicit val actorRefFactory: ActorRefFactory = actorSystem
   implicit val executionContext = actorSystem.dispatcher
   implicit val defaultTimeout: Timeout = Timeout(5.seconds)
@@ -108,9 +114,44 @@ class XmppStream2Test extends FlatSpec with Matchers with ScalaFutures {
       }
         yield (server, client)
 
+    /*
+      <iq id='route:793a6262-4d42-431d-9c89-ad758fe9802d' type='error' xmlns='jabber:client'>
+        <error xmlns='jabber:client'>
+          <undefined-condition xmlns='urn:ietf:params:xml:ns:xmpp-stanzas'/>
+          <text xmlns='urn:ietf:params:xml:ns:xmpp-stanzas'><![CDATA[router: no action succeeded]]></text>
+          <error-condition xmlns='http://wargaming.net/xmpp/router-service#condition'>
+            <report xmlns='http://wargaming.net/xmpp/router-service#condition'><![CDATA[	To-JID: 'wot-clans@$muc-sync.xmppcs.dev'\n	Stanza: [ns: jabber:client; ncn: message]\n	Matched rule:\n		catch_all_case = 'true'\n]]></report>
+          </error-condition>
+        </error>
+      </iq>
+     */
+    val stanzaErrorXml0 = XmppStanzaError.UndefinedCondition().withText("router: no action succeeded").toXml
+    val stanzaErrorXml1 = stanzaErrorXml0.setChildren(
+      stanzaErrorXml0.children :+
+        Element(QName("http://wargaming.net/xmpp/router-service#condition", "error-condition"), Seq(), Seq(
+          Element(QName("http://wargaming.net/xmpp/router-service#condition", "report"), Seq(), Seq(
+            CData("\tTo-JID: 'wot-clans@$muc-sync.xmppcs.dev'\n\tStanza: [ns: jabber:client; ncn: message]\n\tMatched rule:\n\t\tcatch_all_case = 'true'\n")
+          ))
+        ))
+    )
+    val stanza = IQ.create(IQ.Error)
+      .setId("route:793a6262-4d42-431d-9c89-ad758fe9802d")
+      .setChildren(Seq(
+        stanzaErrorXml1
+      ))
+
+    val futStanzaSentAndReceived =
+      for {
+        (server, client) <- futStreamsReopenned
+        _ <- client.sendEvent(StreamEvent.Stanza(stanza))
+        StreamEvent.Stanza(inboundStanza) <- server.recvEvent()
+      }
+        yield (server, client)
+
     whenReady(futBothConnected)(_ =>  println("both connected"))
     whenReady(futStreamsOpenned)(_ => println("both streams openned"))
     whenReady(futTransportsUpgraded)(_ => println("both transports upgraded"))
     whenReady(futStreamsReopenned)(_ => println("both streams reopenned"))
+    whenReady(futStanzaSentAndReceived)(_ => println("stanza sent and received"))
   }
 }
